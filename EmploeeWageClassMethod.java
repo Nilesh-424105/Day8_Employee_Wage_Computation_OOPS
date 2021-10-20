95dcf29b4abe2ad38ee36e6c5bcfc8dbfb136b6c
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-20 13:19:44 +0530

    ADDED: Employye Wage Using Class Method

--- EmploeeWageClassMethod.java
+++ EmploeeWageClassMethod.java
@@ -9,20 +9,18 @@ public class EmploeeWageClassMethod {
 	// constants
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 
-	// instance Variables
-	String company;
-	int empRate;
-	int numOfDays;
-	int maxHrsInMonth;
-
-	private static void computeEmpWageForCompany(String company, int empRate, int numOfDays, int maxHrsInMonth) {
+	// Method Used To Calculate Employee Wage For Company
+	private static void computeEmpWageForCompany() {
 
 		// Variables
 		int empWage = 0, totalEmpHrs = 0, totalEmpWage = 0, totalWorkingDays = 0;
 
 		// computation
-		while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays < numOfDays) {
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
 			int empHrs = 0; // local variable
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -36,7 +34,7 @@ public class EmploeeWageClassMethod {
 			default:
 				empHrs = 0;
 			}
-			empWage = empHrs * empRate;
+			empWage = empHrs * EMP_RATE_PER_HOUR;
 			System.out.println("Employee Wage on Day " + totalWorkingDays + " is " + empWage);
 			totalEmpWage = totalEmpWage + empWage;
 		}
@@ -45,10 +43,9 @@ public class EmploeeWageClassMethod {
 
 	public static void main(String[] args) {
 
-		System.out.println("Employee Wage for company Dmart is: ");
-		computeEmpWageForCompany("DMart", 20, 2, 100);
-		System.out.println("Employee Wage for company Reliance is: ");
-		computeEmpWageForCompany("Reliance", 25, 3, 200);
+		System.out.println("Employee Wage for company is: ");
+		// Method Calling
+		computeEmpWageForCompany();
 
 	}
 
